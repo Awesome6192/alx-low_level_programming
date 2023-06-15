9e3e24c9f9a176dccf178c776d381f079a86651e
Awesome6192 <christianchibuike14@gmail.com>
2023-06-15 13:24:02 -0700

    Task eight

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unstd.h>
+/**
+ * main - A program that prints a line to the standard error
+ * Return: 1 (success)
+ */
+init main(void)
+{
+	fput ("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
