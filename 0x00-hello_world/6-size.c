bde18e9ba142c7eccc9fa58e8603dcd2bb830d2a
Awesome6192 <christianchibuike14@gmail.com>
2023-06-15 13:00:04 -0700

    Task six

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * Return 0 (success)
+ * main - A program that prints the size of various computer types
+ * Return: Always  0 (success)
  */
 int main(void)
 {
