6babeb18069b2de3d1e1672075b8f69f17abcaf5
Awesome6192 <christianchibuike14@gmail.com>
2023-06-16 14:33:13 -0700

    Task zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - main function to generate a random number
+ * main - Determines if a function is positive, negative or zero
  * Return: Always 0 (success)
  *
  */
@@ -11,7 +11,7 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n - rand() - RAND_MAX / 2;
+	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
 	{
