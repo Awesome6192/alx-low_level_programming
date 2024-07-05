6ba77b89fa04b367cf6970917a5cb42cb6acaedd
Awesome6192 <christianchibuike14@gmail.com>
2024-07-05 18:38:41 +0100

    good job

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -4,8 +4,8 @@
 - sends a request to the URL and displays the value
 - of the X-Request-Id variable found in the header ofthe response.
 """
+import request
 import sys
-import urllib.request
 
 if __name__ == "__main__":
     url = sys.argv[1]
